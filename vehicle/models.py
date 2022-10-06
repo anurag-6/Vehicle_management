dab5b0f52a2c78f55f1fc0e54384ef2878f97715
anurag-6 <anuragr8129@gmail.com>
2022-10-06 12:23:13 +0530

    a change

--- vehicle/models.py
+++ vehicle/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 
-COLOR_CHOICES = (
+VEHICLE_CHOICES = (
 
     ('Two Wheeler','2 Wheeler'),
     ('Three Wheeler', '3 Wheeler'),
@@ -14,7 +14,7 @@ COLOR_CHOICES = (
 
 class Vehicle(models.Model):
     vh_number = models.CharField(max_length=20,verbose_name="Vehicle Number",blank=False)
-    vh_type = models.CharField(max_length=20,verbose_name="Vehicle Type", choices=COLOR_CHOICES,blank=False)
+    vh_type = models.CharField(max_length=20,verbose_name="Vehicle Type", choices=VEHICLE_CHOICES,blank=False)
     vh_model = models.CharField(max_length=20,verbose_name="Vehicle Model",blank=False)
     vh_disc = models.TextField(verbose_name="Vehicle Discription",blank=False)
 
